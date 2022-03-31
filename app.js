5657f016e70301d0c5e718826a867181d1077c34
Rob Lawrenson <roblawrenson@hotmail.co.uk>
2022-03-31 14:44:28 +0100

    added function playSound

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+let keyDownClick = ""
+// place var outside eventListener for function to access
 document.addEventListener("keydown", (event) => {
 
-    let keyDownClick = ""
+    
     let keyNumber = event.key
     let key1 = document.getElementById("boom");
     let key2 = document.getElementById("clap");
@@ -10,11 +12,11 @@ document.addEventListener("keydown", (event) => {
         // hmm how do you restart the audio everytime the key is pressed?
 
         keyDownClick = key1
-        keyDownClick.pause()
-        keyDownClick.currentTime = 0 // CAPITAL T!! tada moment...
-        keyDownClick.play()
+        // keyDownClick.pause()
+        // keyDownClick.currentTime = 0
+        // keyDownClick.play()
 
-        // playSound()
+        playSound()
 
     } else if (keyNumber = 2) {
         key2.pause()
@@ -25,8 +27,8 @@ document.addEventListener("keydown", (event) => {
 })
 
 
-// function playSound() {
-//     key1.pause()
-//     key1.currentTime = 0 // CAPITAL T!! tada moment...
-//     key1.play()
-// }
+function playSound() {
+    keyDownClick.pause()
+    keyDownClick.currentTime = 0
+    keyDownClick.play()
+}
