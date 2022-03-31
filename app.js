bbc61fb12562e3db2d219cc869d6904b419398e8
Rob Lawrenson <roblawrenson@hotmail.co.uk>
2022-03-31 14:27:38 +0100

    renamed ids to file name

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 document.addEventListener("keydown", (event) => {
     let keyNumber = event.key
-    let key1 = document.getElementById("key1");
-    let key2 = document.getElementById("key2");
+    let key1 = document.getElementById("boom");
+    let key2 = document.getElementById("clap");
     // keyPressed.innerHTML = keyNumber
     
     if (keyNumber == 1) {
@@ -12,7 +12,7 @@ document.addEventListener("keydown", (event) => {
         key1.play()
 
         // playSound()
-        
+
     } else if (keyNumber = 2) {
         key2.pause()
         key2.currentTime = 0 // CAPITAL T!! tada moment...
