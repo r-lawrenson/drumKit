5204c82f12910b28ee4027f18e6d2cbaf9d8c5a9
Rob Lawrenson <roblawrenson@hotmail.co.uk>
2022-03-31 14:22:13 +0100

    added else for key number 2

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 document.addEventListener("keydown", (event) => {
     let keyNumber = event.key
     let key1 = document.getElementById("key1");
-
+    let key2 = document.getElementById("key2");
     // keyPressed.innerHTML = keyNumber
     
     if (keyNumber == 1) {
         // hmm how do you restart the audio everytime the key is pressed?
 
-        playSound()
-    } else {}
+        key1.pause()
+        key1.currentTime = 0 // CAPITAL T!! tada moment...
+        key1.play()
+
+        // playSound()
+        
+    } else if (keyNumber = 2) {
+        key2.pause()
+        key2.currentTime = 0 // CAPITAL T!! tada moment...
+        key2.play()
+    }
 
 })
 
 
-function playSound() {
-    key1.pause()
-    key1.currentTime = 0 // CAPITAL T!! tada moment...
-    key1.play()
-}
+// function playSound() {
+//     key1.pause()
+//     key1.currentTime = 0 // CAPITAL T!! tada moment...
+//     key1.play()
+// }
