e8998bcdad8fbf72acb8a403965dac4ea8de3176
Rob Lawrenson <roblawrenson@hotmail.co.uk>
2022-04-01 17:32:49 +0100

    added click event

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 let audioFile = "";
-let keyDownPress = "";
+let keyPressClick = "";
 // place var outside eventListener for function to access
-
 let key1 = document.getElementById("boom");
 let key2 = document.getElementById("clap");
 let key3 = document.getElementById("hihat");
@@ -12,58 +11,57 @@ let key7 = document.getElementById("snare");
 let key8 = document.getElementById("tink");
 let key9 = document.getElementById("tom");
 
-// document.addEventListener("click", (event) => {
-//     let click = event.target // get event
-//     keyDownPress = click // send event to outside variable keydown press
-//     checkIf(); 
-// });
+document.addEventListener("click", (event) => {
+    click = (event.target);
+    keyPressClick = click.id // really?
+    checkIf()
+});
 
 document.addEventListener("keydown", (event) => {
-    let keyDown = event.key // get event
-    keyDownPress = keyDown // send event to outside variable keydown press
+    let keyDown = event.key
+    keyPressClick = keyDown // send to outside variable
     checkIf(); 
 });
 
 function checkIf() {
-    if (keyDownPress == 1) {
+    if (keyPressClick == 1) {
         audioFile = key1
         playSound();
-    } else if (keyDownPress == 2) {
+    } else if (keyPressClick == 2) {
         audioFile = key2
         playSound();
-    } else if (keyDownPress == 3) {
+    } else if (keyPressClick == 3) {
         audioFile = key3
         playSound();
-    } else if (keyDownPress == 4) {
+    } else if (keyPressClick == 4) {
         audioFile = key4
         playSound();
-    } else if (keyDownPress == 5) {
+    } else if (keyPressClick == 5) {
         audioFile = key5
         playSound();
-    } else if (keyDownPress == 6) {
+    } else if (keyPressClick == 6) {
         audioFile = key6
         playSound();
-    } else if (keyDownPress == 7) {
+    } else if (keyPressClick == 7) {
         audioFile = key7
         playSound();
-    } else if (keyDownPress == 8) {
+    } else if (keyPressClick == 8) {
         audioFile = key8
         playSound();
-    } else if (keyDownPress == 9) {
+    } else if (keyPressClick == 9) {
         audioFile = key9
         playSound();
-        }
-        clearVars()
+    }
+    clearVars()
 };
 
 function playSound() {
     audioFile.pause()
     audioFile.currentTime = 0
     audioFile.play()
-    
 };
 
 function clearVars() {
     audioFile = "";
-    keyDownPress = "";
-}
+    keyPressClick = "";
+};
